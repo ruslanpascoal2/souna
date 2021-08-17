0e738998b0285fa5d138eeec1bc697d2cef66007
Ruslan <61955096+ruslanpascoal2@users.noreply.github.com>
2021-08-16 22:54:09 -0300

    key mapping

--- src/music/Notes.js
+++ src/music/Notes.js
@@ -3,4 +3,21 @@ const notes_3 = ["C3", "D3", "E3", "F3", "G3", "A3", "B3"];
 const notes_4 = ["C4", "D4", "E4", "F4", "G4", "A4", "B4"];
 const notes_5 = ["C5", "D5", "E5", "F5", "G5", "A5", "B5"];
 
+export const keys = [
+    { note: "C3", blackKeyNext: "C#3", keyMap: "z" },
+    { note: "D3", blackKeyNext: "D#3", keyMap: "x" },
+    { note: "E3", blackKeyNext: false, keyMap: "c" },
+    { note: "F3", blackKeyNext: "F#3", keyMap: "v" },
+    { note: "G3", blackKeyNext: "G#3", keyMap: "b" },
+    { note: "A3", blackKeyNext: "A#3", keyMap: "n" },
+    { note: "B3", blackKeyNext: false, keyMap: "m" },
+    { note: "C4", blackKeyNext: "C#4", keyMap: "," },
+    { note: "D4", blackKeyNext: "D#4", keyMap: "." },
+    { note: "E4", blackKeyNext: false, keyMap: "a" },
+    { note: "F4", blackKeyNext: "F#4", keyMap: "s" },
+    { note: "G4", blackKeyNext: "G#4", keyMap: "d" },
+    { note: "A4", blackKeyNext: "A#4", keyMap: "f" },
+    { note: "B4", blackKeyNext: false, keyMap: "g" }
+];
+
 export const Notes = [...notes_2, ...notes_3, ...notes_4, ...notes_5];
--- src/music/components/Keyboard.js
+++ src/music/components/Keyboard.js
@@ -1,54 +1,86 @@
 import './Keyboard.scss';
+import { keys } from "../Notes";
+import * as Tone from 'tone'
+import { useEffect } from 'react';
+import { distinct, distinctUntilChanged, distinctUntilKeyChanged, fromEvent, map, tap } from 'rxjs';
 
 export function Keyboard() {
+    let synth;
+    useEffect(() => {
+        synth = new Tone.AMSynth().toDestination();
 
-    const keys = [
-        { note: "C2", blackKeyNext: true },
-        { note: "D2", blackKeyNext: true },
-        { note: "E2", blackKeyNext: false },
-        { note: "F2", blackKeyNext: true },
-        { note: "G2", blackKeyNext: true },
-        { note: "A2", blackKeyNext: true },
-        { note: "B2", blackKeyNext: false },
-        { note: "C3", blackKeyNext: true },
-        { note: "D3", blackKeyNext: true },
-        { note: "E3", blackKeyNext: false },
-        { note: "F3", blackKeyNext: true },
-        { note: "G3", blackKeyNext: true },
-        { note: "A3", blackKeyNext: true },
-        { note: "B3", blackKeyNext: false }
-    ];
+        fromEvent(document, "keydown").pipe(
+            map(x => x.key),
+            distinctUntilChanged()
+        )
+        .subscribe((ev) => {
+            playNote(null, ev)
+        })
+
+        fromEvent(document, "keyup").pipe(
+            distinctUntilChanged()
+        )
+        .subscribe(() => stop())
+    })  
+
+    const playNote = (note, keyPressed) => {
+        let _note;
+
+        if(keyPressed){
+            let match = keys.find(x => x.keyMap === keyPressed);
+            _note = match ? match.note : null; 
+        }
+        else{
+            _note = note;
+        }
+
+        if(!_note) return;
+        synth = new Tone.AMSynth().toDestination();
+        synth.triggerAttack(_note, "8n");
+    }
+
+    const stop = () => {
+        synth.dispose();
+    }
 
     return (
-        <div className="card flex justify-center flex-col items-center" style={keyboardStyles.general}>
-            <div style={keyboardStyles.controlSection} className="flex">
-                <div className="flex flex-col" style={{ width: "30%", borderRight: "2px solid #8d8d8d" }}>
-                    <div className="p-6">
-                        <img src="roland.png" style={{ filter: 'opacity(0.5)', height: "50px" }}></img>
+            <div
+            onKeyDown={(event) => playNote(event)} 
+            id="keyboard" className="card flex justify-center flex-col items-center" style={keyboardStyles.general}>
+                <div style={keyboardStyles.controlSection} className="flex">
+                    <div className="flex flex-col" style={{ width: "30%", borderRight: "2px solid #8d8d8d" }}>
+                        <div className="p-6">
+                            <img src="roland.png" style={{ filter: 'opacity(0.5)', height: "50px" }}></img>
+                        </div>
+                        <div style={{ height: "6px", width: "100%", background: "#8e8e8e" }}></div>
                     </div>
-                    <div style={{ height: "6px", width: "100%", background: "#8e8e8e" }}></div>
+                    <div className="flex flex-col" style={{ width: "70%", }}>
+                        <div className="p-6">
+                            <div style={{ height: "50px" }}></div>
+                        </div>
+                        <div style={{ height: "6px", width: "100%", background: "rgb(245 129 35 / 40%)" }}></div>
+                    </div>
                 </div>
-                <div className="flex flex-col" style={{ width: "70%", }}>
-                    <div className="p-6">
-                        <div style={{ height: "50px" }}></div>
+                <div className="flex" style={keyboardStyles.keysSection}>
+                    <div style={keyboardStyles.keys}>
+                        {
+                            keys.map(x => <div key={x.note} className="key-wrapper">
+                                <div 
+                                onMouseUp={stop} 
+                                onMouseDown={() => playNote(x.note)}
+                                style={keyboardStyles.keys.key} className="key">
+                                </div>
+                                {
+                                    x.blackKeyNext && 
+                                    <div className="black-key"
+                                    onMouseUp={stop} onMouseDown={() => playNote(x.blackKeyNext)}
+                                    style={keyboardStyles.keys.blackKeys}></div>
+                                }
+                            </div>)
+                        }
                     </div>
-                    <div style={{ height: "6px", width: "100%", background: "rgb(245 129 35 / 40%)" }}></div>
                 </div>
             </div>
-            <div className="flex" style={keyboardStyles.keysSection}>
-                <div style={keyboardStyles.keys}>
-                    {
-
-                        keys.map(x => <div key={x.note} className="key-wrapper">
-                            <div style={keyboardStyles.keys.key} className="key"></div>
-                            {
-                                x.blackKeyNext && <div className="black-key" style={keyboardStyles.keys.blackKeys}></div>
-                            }
-                        </div>)
-                    }
-                </div>
-            </div>
-        </div>
     )
 }
 
